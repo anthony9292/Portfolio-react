c0dd8e1e1313a285352a70326f13be6e24a74daf
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-22 23:31:15 -0400

    finished adding hub everything works

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -12,7 +12,7 @@ export const Projects = [
      id: 2,
      title: "Cup of Sugar Web app",
      img:  "./asset/cupfullfruites.jpg",
-     url:""
+     url:"https://github.com/anthony9292/cupofsugar",
     
      
 
@@ -21,12 +21,14 @@ export const Projects = [
     { 
         id:3,
         title: "tech blog app",
-        img: "./asset/tech.jpg"
+        img: "./asset/tech.jpg",
+        url:"https://github.com/anthony9292/tech-blog",
     },
     {  
         id: 4,
         title: "Employee hub",
         img: "./asset/teamgenerator.jpg",
+        url:"https://github.com/anthony9292/employe-Hub",
 
 
     }, 
@@ -35,44 +37,89 @@ export const Projects = [
         id: 5,
         title: "fit tracker",
         img: "./asset/fitness.jpg",
+        url:"https://github.com/anthony9292/fit-tracker",
     },
 
        
     {
         id: 6,
         title: "budget tracker",
-        img: "./asset/budget.png"
+        img: "./asset/budget.png",
+        url:"https://github.com/anthony9292/budget-tracker",
 
     },
 
     {
         id: 7, 
         title: "HTML Portfolio",
-        img: "./asset/portfolioart.png"
+        img: "./asset/portfolioart.png",
+        url:"https://github.com/anthony9292/Anthony-Ndegwa-Portfolio",
     }, 
 
     {
         id:8, 
         title: "note maker app",
         img: "./asset/pink.jpg",
+        url:"https://github.com/anthony9292/Note-maker",
 
     },
     { 
         id:9,
         title: "ecommerce app",
-        img: "./asset/ecommerce.png"
+        img: "./asset/ecommerce.png",
+        url:"https://github.com/anthony9292/e-commerce",
 
      },
 
 
 ];
     export const NetworkHub =  [
-     
+        
+    {
+        Id:1,
+        title:"Linkedin",
+        img:"./asset/linkedin.png",
+        url:"https://www.linkedin.com/in/anthony-ndegwa-70213b127/",
+
+    },
+
+      {
+          id:2,
+          title:"github",
+          img:"./asset/github.png",
+          url:"https://github.com/anthony9292",
+      },
+  
 
     
     ];
 
      export const Socials = [
+
+
+        {
+            Id:1,
+            title:"Gmail",
+            img:"./asset/linkedin.png",
+            url:"https://www.linkedin.com/in/anthony-ndegwa-70213b127/",
+    
+        },
+        {
+            Id:1,
+            title:"Twitter",
+            img:"./asset/twitter.png",
+            url:"https://www.linkedin.com/in/anthony-ndegwa-70213b127/",
+    
+        },
+
+        {
+            Id:1,
+            title:"instagram",
+            img:"./asset/instagram.jpg",
+            url:"https://www.instagram.com/unknowndude24/",
+    
+        },
+        
        
 
       
